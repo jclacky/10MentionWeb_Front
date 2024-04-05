69409335a16f5548c8dab51e029e027ee9603c15
Obriän <jclacky4@gmail.com>
2024-04-05 16:49:36 +0200

    modifié

--- 03_javaScript/03_projet/i_loveJs/assets/js/script.js
+++ 03_javaScript/03_projet/i_loveJs/assets/js/script.js
@@ -1,57 +1,112 @@
-// Dark mode
+// --------------------- Dark mode -----------------//
 
-let switchBox = document.querySelector('.switch'); // Je stocke l'élément div.switch
+let switchBox = document.querySelector('.switch'); // je stock l'élément div.switch
 
-let container = document.querySelector('.container-fluid'); // je stocke l'élément div.container
+let container = document.querySelector('.container-fluid'); // je stock l'élément div.container
 
- switchBox.addEventListener('click', changeMode ); // Je déclare l'évènement  click sur la div.switch
+switchBox.addEventListener('click', changeMode);// je déclare l'événement click sur la div.switch 
 
- let nav = document.querySelector('.navbar-desktop');
+let nav = document.querySelector('.navbar-desktop');
+
 let liens = document.querySelectorAll('.navbar-desktop a');
-console.log(liens);
 
-let btn_change = document.querySelector('.btn')
-let icon = document.querySelector('.switch i')
-let h1_change = document.querySelector('h1');
-let btn = document.querySelector('.btn')
-let icon1 = document.querySelector('block-bnt i')
-icon1.addEventListener('click', changeMode);
+let btnChange = document.querySelector('.btn');
+
+let icon = document.querySelector('.switch i');
+
+let titre = document.querySelector('h1');
+
+
 function changeMode(){
-    nav.classList.toggle('nav-dark');
-    //la div container change de couleur de fond
-    container.classList.toggle('container-change');
-    
-    h1_change.classList.toggle('h1-change');
 
-    for (let lien of liens){
+    //  la div container ch&nge de couleur de fond
+    container.classList.toggle('container-change');
+    nav.classList.toggle('nav-dark');
+
+    for (let lien of liens) {
+        
         lien.classList.toggle('lienDarkMode');
     }
-    btn_change.classList.toggle('btn-change');
-    icon.classList.toggle('icon-change')
-    btn_change.classList.toggle('btn-dark')
-    btn_change.classList.toggle('btn-dark:hover')
 
+    // je déplace le bouton 
+    btnChange.classList.toggle('btn-change');
+    //je change la couleur du bouton
+    btnChange.classList.toggle('btn-dark');
+
+    icon.classList.toggle('icone-change');
 
     if (icon.classList.contains('bi-sun-fill')) {
 
         icon.classList.remove('bi-sun-fill');
-        icon.classList.add('bi-moon-fill');
-
+        icon.classList.add("bi-moon-fill");
         
-    }
-    else{
+    } else {
         icon.classList.remove('bi-moon-fill');
-        icon.classList.add('bi-sun-fill')
+        icon.classList.add("bi-sun-fill");  
     }
-    switchBox.classList.toggle('switch-change')
-    
-    if(icon1.classList.contains('bi bi-emoji-neutral')){
-        icon.classList.remove('bi bi-emoji-neutral');
-        icon.classList.add('bi bi-emoji-wink');
+
+    switchBox.classList.toggle('switch-change');
+
+    titre.classList.toggle('titre');
+
+}
+
+// ---------------------------------- Partie video ---------------------------------- //
+        // -------------------------- Icon smile -------------------------- //
+
+
+
+// 1ère façon de faire
+// let iconeSmile = document.querySelector('.bi-emoji-neutral');
+
+// if (iconeSmile.classList.contains('bi-emoji-neutral')) {
+//     iconeSmile.classList.remove('bi-emoji-neutral');
+//     iconeSmile.classList.add('bi-')
+// } 
+
+
+// 2ème façon de faire
+let emoji = document.querySelector('.bloc-btn i')
+emoji.addEventListener('click', ()=> {
+    emoji.classList.toggle('bi-emoji-wink');
+});
+
+
+        // -------------------------- Bouton abonnez-vous -------------------------- //
+
+
+// let btnAbonner = document.querySelector('.btn-abonner');
+// btnAbonner.addEventListener('click', );
+// btnAbonner.innerText = 'Abonner ✓';
+
+// 1ère façon de faire
+// let btnAbonner = document.querySelector('.btn-abonner')
+// btnAbonner.addEventListener('click', ()=> {
+// btnAbonner.classList.toggle(btnAbonner.innerText = 'Abonner ✓');
+// });
+
+
+// 2ème façon de faire
+let btnAbonne = document.querySelector('.btn-abonner');
+
+btnAbonne.addEventListener('click', ()=> {
+
+    if( btnAbonne.innerText === 'Abonnez-vous') {
+
+        btnAbonne.innerHTML = 'Abonné <i class="bi bi-check"></i>';
+
+    } else {
+
+        btnAbonne.innerText = 'Abonnez-vous';
     }
-    else {
-        icon.classList.add('bi bi-emoji-wink');
-        icon.classList.remove('bi bi-emoji-neutral');
-    }
- }
+});
+
+
+// ------------------ Cookies ------------------
+
+let btnCookies = document.querySelector('.btn-success');
+ let cookies = document.querySelector('.cookies');
+ btnCookies.addEventListener('click', ()=>{
+    cookies.styles.bottom ='auto';
+ })
 
