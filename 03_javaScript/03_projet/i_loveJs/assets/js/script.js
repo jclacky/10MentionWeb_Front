fd49e025702ee998de44500f9271156f6edce6c0
Obriän <jclacky4@gmail.com>
2024-04-19 14:19:14 +0200

    update

--- 03_javaScript/03_projet/i_loveJs/assets/js/script.js
+++ 03_javaScript/03_projet/i_loveJs/assets/js/script.js
@@ -51,62 +51,133 @@ function changeMode(){
 
 }
 
-// ---------------------------------- Partie video ---------------------------------- //
-        // -------------------------- Icon smile -------------------------- //
+
+// ------------------------- Partie video --------------------//
+        //------------------ icone smile------------- //
+
+    let iconeSmile = document.querySelector('.bi-emoji-neutral');
+
+    iconeSmile.addEventListener('click',()=>{
+
+        //1ére méthode
+
+        if (iconeSmile.classList.contains('bi-emoji-neutral')) {
+
+            iconeSmile.classList.remove('bi-emoji-neutral');
+            iconeSmile.classList.add("bi-emoji-heart-eyes-fill");
+            
+        } else {
+            iconeSmile.classList.remove('bi-emoji-heart-eyes-fill');
+            iconeSmile.classList.add("bi-emoji-neutral");  
+        }
+
+        //2éme méthode
+
+        // iconeSmile.classList.toggle('bi-emoji-wink-fill');
+
+        
+    } )
 
 
+    //------------------  Bouton abonnez-vous------------- //
 
-// 1ère façon de faire
-// let iconeSmile = document.querySelector('.bi-emoji-neutral');
+    let btnAbonne = document.querySelector('.btn-abonner');
 
-// if (iconeSmile.classList.contains('bi-emoji-neutral')) {
-//     iconeSmile.classList.remove('bi-emoji-neutral');
-//     iconeSmile.classList.add('bi-')
-// } 
+    btnAbonne.addEventListener('click', ()=>{
 
+        if (btnAbonne.innerText === 'Abonnez-vous') {
 
-// 2ème façon de faire
-let emoji = document.querySelector('.bloc-btn i')
-emoji.addEventListener('click', ()=> {
-    emoji.classList.toggle('bi-emoji-wink');
-});
+            btnAbonne.innerHTML = 'Abonné <i class="bi bi-check"></i>';
+             
+        } else {
+            
+            btnAbonne.innerText = 'Abonnez-vous';
+        }
 
-
-        // -------------------------- Bouton abonnez-vous -------------------------- //
+    })
 
 
-// let btnAbonner = document.querySelector('.btn-abonner');
-// btnAbonner.addEventListener('click', );
-// btnAbonner.innerText = 'Abonner ✓';
+    //------------------  Cookies ------------- //
 
-// 1ère façon de faire
-// let btnAbonner = document.querySelector('.btn-abonner')
-// btnAbonner.addEventListener('click', ()=> {
-// btnAbonner.classList.toggle(btnAbonner.innerText = 'Abonner ✓');
-// });
+    let btnCookies = document.querySelector('.btn-success');
+
+    let cookies = document.querySelector('.cookies');
+    btnCookies.addEventListener('click', ()=> {
+
+            // cookies.style.opacity = "0";
+            cookies.style.bottom = "auto" ;
+            
+
+    })
 
 
-// 2ème façon de faire
-let btnAbonne = document.querySelector('.btn-abonner');
+     //------------------  carrousel ------------- //
 
-btnAbonne.addEventListener('click', ()=> {
+let left = document.querySelector('.left');
+let automatic = document.querySelector('.automatic');
+let right = document.querySelector('.right');
+let img = document.querySelector('img');
+let automaticIcon = document.querySelector('.automatic i');
 
-    if( btnAbonne.innerText === 'Abonnez-vous') {
+let i = 1; 
 
-        btnAbonne.innerHTML = 'Abonné <i class="bi bi-check"></i>';
+right.addEventListener('click', carrousel);
 
-    } else {
-
-        btnAbonne.innerText = 'Abonnez-vous';
+function carrousel (){
+    
+    i++; // on incrémente puis on affecte : la valeur de la variable sera donc 1 / puis 2 /puis 3/ etc.
+    
+    if (i == 7) {
+        i = 1 ;
+        img.setAttribute('src', `assets/img/${i}.jpg`);    
     }
-});
+    img.setAttribute('src', `assets/img/${i}.jpg`);
+    // j'appelle de façon automatique les images par leur nom 
+
+} 
 
 
-// ------------------ Cookies ------------------
+left.addEventListener('click', ()=>{
+    i--;
+    if (i == 0) {
 
-let btnCookies = document.querySelector('.btn-success');
- let cookies = document.querySelector('.cookies');
- btnCookies.addEventListener('click', ()=>{
-    cookies.styles.bottom ='auto';
- })
+        i = 6 ;
+        img.setAttribute('src', `assets/img/${i}.jpg`);
+        
+    }
+    img.setAttribute('src', `assets/img/${i}.jpg`);
+})
 
+
+
+let statut = null; // Initialisation d'une variable pour stocker l'état du slider (lecture ou pause)
+automatic.addEventListener('click', ()=>{
+
+    // Inversion de la classe 'bi-pause-fill' sur l'élément automaticIcon à chaque clic
+    automaticIcon.classList.toggle('bi-pause-fill');
+
+    // Vérification de l'état actuel du slider
+    if (statut == null ) {
+         // Si le slider est en pause
+        // Activation du mode lecture : exécution de la fonction carrousel toutes les 1500 millisecondes (1.5 secondes)
+
+        statut = window.setInterval(carrousel, 1500);  //setInterval est utilisé pour exécuter une fonction à intervalles réguliers
+  
+    } else {
+        // Si le slider est en lecture
+        // Désactivation du mode lecture : arrêt de l'exécution de la fonction carrousel à intervalles réguliers
+        window.clearInterval(statut);
+
+        //clearInterval est utilisé pour arrêter l'exécution de cette fonction à intervalles réguliers en utilisant l'identifiant de l'intervalle retourné par setInterval.
+
+
+        statut = null; // Réinitialisation de la variable statut à null pour indiquer que le slider est en pause
+        
+    }
+})
+// --------------Loader -----------------
+
+let loader = document. querySelector('#loader');
+window.addEventListener('load', ()=>{
+    loader.classList.add('hideLoader');
+})
